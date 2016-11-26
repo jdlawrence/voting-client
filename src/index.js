2299a3771243af1fe29cee7c5b1d9befb7cb3cb6
jdlawrence <jamil.lawrence@gmail.com>
2016-11-25 23:18:45 -0800

    Working on Voting component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+
+
 ReactDOM.render(
   <Voting pair={pair} />,
+  // <h1>Hello World!</h1>,
   document.getElementById('app')
 );
 
