d004a5c1080b88a59bd04286431c347d2c23aeac
jdlawrence <jamil.lawrence@gmail.com>
2016-12-03 22:11:44 -0800

    Working on implementing voting button callbacks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
@@ -17,6 +18,11 @@ store.dispatch({
       tally: {Sunshine: 2}
     }
   }
+});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state => {
+  store.dispatch({type: 'SET_STATE', state})
 });
 
 const routes = <Route component={App}>
